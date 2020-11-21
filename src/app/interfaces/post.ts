8fce820530c7a2295631681f692402a35bc4378d
victoriakaramanova <37152322+victoriakaramanova@users.noreply.github.com>
2020-11-21 21:34:53 +0200

    initial

---
+++ src/app/interfaces/post.ts
@@ -0,0 +1,11 @@
+import { IBase } from './base';
+import { ITheme } from './theme';
+import { IUser } from './user';
+
+export interface IPost extends IBase {
+    likes: string[];
+    text: string;
+    userId: IUser;
+    themeId: ITheme;
+    
+}
